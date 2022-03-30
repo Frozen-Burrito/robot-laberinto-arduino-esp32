7c48d0c795299d0f89ee4608a7555ffdf9682607
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-03-30 12:56:24 -0600

    Mediciones del sensor MPU6050

--- Practica3_EstructuraYAvance.ino
+++ Practica3_EstructuraYAvance.ino
@@ -1,3 +1,11 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+// Declaracion del sensor MPU6050 con libreria de Adafruit.
+const Adafruit_MPU6050 mpu6050;
+const bool usarMPU6050 = false;
+
 // Pin de activacion / desactivacion del movimiento.
 const int PIN_EN_MOV = 24;
 
@@ -23,8 +31,21 @@ pines_motor_t motor2 = { 4, 5, 10 }; // Numeros de pines para el motor derecho.
 // Declaracion de la funcion cambiarMovimientoMotor.
 void cambiarMovimientoMotor(const int, const pines_motor_t&, const int velocidad = 0);
 
-void setup() {
+void configurarMPU6050();
 
+void setup() 
+{
+  // Solo inicializar Serial si el Arduino va a tener conexion USB y se desea utilizar Serial.
+  if (usarSerial)
+  {
+    Serial.begin(9600);
+    while (!Serial) 
+    {
+    }
+  
+    Serial.println("Serial listo");
+  }
+  
   // Configurar pines de control de motores como salida.
   pinMode(motor1.pin1, OUTPUT);
   pinMode(motor1.pin2, OUTPUT);
@@ -37,16 +58,14 @@ void setup() {
   // Pin de activacion como entrada.
   pinMode(PIN_EN_MOV, INPUT);
 
-  // Solo inicializar Serial si el Arduino va a tener conexion USB y se desea utilizar Serial.
-  if (usarSerial)
+  Serial.println("Motores inicializados");
+
+  if (usarMPU6050 && !mpu6050.begin())
   {
-    Serial.begin(9600);
-    while (!Serial) 
-    {
-    }
-  
-    Serial.println("Serial listo");
+    Serial.println("Error de inicializacion del MPU6050: verifica que este conectado");
   }
+
+  Serial.println("MPU6050 inicializado");
 
   delay(1000);
 }
@@ -55,6 +74,28 @@ void loop()
 {
   // Obtener valor 
   int movimientoActivado = digitalRead(PIN_EN_MOV);
+
+  if (usarMPU6050)
+  {
+    // Configurar el MPU6050.
+    configurarMPU6050();
+
+    // Obtener valores del MPU6050.
+    sensors_event_t a, g, t;
+    mpu6050.getEvent(&a, &g, &t);
+
+    if (usarSerial)
+    {
+      // Imprimir valores de acelerometro, giroscopio y temperatura.
+      String valorAcc = "(" + String(a.acceleration.x) + "," + String(a.acceleration.y) + ", " + String(a.acceleration.z) + ")";
+      Serial.println("Aceleración (x,y,z): " + valorAcc + " m/s^2");
+
+      String valorGyro = "(" + String(g.gyro.x) + "," + String(g.gyro.y) + ", " + String(g.gyro.z) + ")";
+      Serial.println("Rotacion (x,y,z): " + valorGyro + " rad/s");
+
+      Serial.println("Temperatura: " + String(t.temperature) + " °C");
+    }
+  }
 
   if (movimientoActivado == HIGH)
   {
@@ -78,46 +119,79 @@ void loop()
     cambiarMovimientoMotor(MOTOR_DETENIDO, motor2);
     delay(1000);
   }
+}
 
-  /*
-  if (btnAvanzar == HIGH && estadoMotor != 1) 
+// Esta funcion configura el rango del acelerometro, el rango del giroscopio y el
+// ancho de banda del filtro del sensor MPU6050. Muestra mensajes de confirmacion en
+// el monitor serial, si esta activado.
+void configurarMPU6050()
+{
+  mpu6050.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu6050.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu6050.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  if (usarSerial)
   {
-    estadoMotor = 1;
+    Serial.print("Rango del acelerometro: ");
+    switch (mpu6050.getAccelerometerRange()) 
+    {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+    }
     
-    digitalWrite(ENABLE_MOTOR1, HIGH);
-    digitalWrite(PIN1_MOTOR1, HIGH);
-    digitalWrite(PIN2_MOTOR1, LOW);
-
-    lcd.clear();
-    lcd.print("Avanzando");
+    Serial.print("Rango del giroscopio: ");
+    switch (mpu6050.getGyroRange()) 
+    {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+    }
     
-    Serial.println("Avanzando");
-    
-  } else if (btnRetroceder == HIGH && estadoMotor != 2) 
-  {
-    estadoMotor = 2;
-     
-    digitalWrite(ENABLE_MOTOR1, HIGH);
-    digitalWrite(PIN1_MOTOR1, LOW);
-    digitalWrite(PIN2_MOTOR1, HIGH);
-
-    lcd.clear();
-    lcd.print("Retrocediendo");
-    Serial.println("Retrocediendo");
-    
-  } else if (btnDetener == HIGH && estadoMotor != 0) 
-  {
-    estadoMotor = 0;
-    
-    digitalWrite(ENABLE_MOTOR1, LOW);
-    digitalWrite(PIN1_MOTOR1, LOW);
-    digitalWrite(PIN2_MOTOR1, LOW);
-
-    lcd.clear();
-    lcd.print("Detenido");
-    Serial.println("Detenido");
+    Serial.print("Ancho de banda de filtro: ");
+    switch (mpu6050.getFilterBandwidth()) 
+    {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+    }
   }
-  */
 }
 
 // Definicion de la funcion cambiarMovimientoMotor.

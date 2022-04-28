a3d663582134a225b11ffa4834f930ec58c35916
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-04-27 21:27:02 -0500

    Soporte para ESP32, deteccion de colores RGB

--- Practica3_EstructuraYAvance.ino
+++ Practica3_EstructuraYAvance.ino
@@ -2,8 +2,15 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
+#define PWM1_CH1  0
+#define PWM1_CH2  0
+#define PWM1_RES  8
+#define PWM1_FREQ 1000
+
+#define VALOR_MAX_COLOR 5000
+
 // Declaracion del sensor MPU6050 con libreria de Adafruit.
-const Adafruit_MPU6050 mpu6050;
+Adafruit_MPU6050 mpu6050;
 const bool usarMPU6050 = false;
 
 // Pin de activacion / desactivacion del movimiento.
@@ -25,13 +32,43 @@ struct pines_motor_t
   const int pin1;
   const int pin2;
   const int pinEn;
+  const int canalPWM;
 };
 
-pines_motor_t motor1 = { 2, 3, 9 }; // Numeros de pines para el motor izquierdo.
-pines_motor_t motor2 = { 4, 5, 10 }; // Numeros de pines para el motor derecho.
+typedef struct {
+  const int s0;
+  const int s1;
+  const int s2;
+  const int s3;
+  const int out;
+} tcs3200_t;
 
+typedef struct {
+  const int r;
+  const int g;
+  const int b;
+} rgb_t;
+
+typedef enum {
+  BLANCO,
+  ROJO,
+  AMARILLO,
+  VERDE,
+  AZUL,
+  NEGRO
+} color_t;
+
+pines_motor_t motor1 = { 2, 3, 9, PWM1_CH1 }; // Numeros de pines para el motor izquierdo.
+pines_motor_t motor2 = { 4, 5, 10, PWM1_CH2 }; // Numeros de pines para el motor derecho.
+
+tcs3200_t sensor_color = { 27, 26, 32, 33, 23 }; // Numeros de pines del sensor TCS 3200.
+
+void setupMotor(const pines_motor_t&);
 // Declaracion de la funcion cambiarMovimientoMotor.
 void cambiarMovimientoMotor(const int, const pines_motor_t&, const int velocidad = 0);
+
+void setup_tcs3200(const tcs3200_t&);
+rgb_t medir_color(const tcs3200_t&);
 
 void configurarMPU6050();
 
@@ -48,14 +85,11 @@ void setup()
     Serial.println("Serial listo");
   }
   
-  // Configurar pines de control de motores como salida.
-  pinMode(motor1.pin1, OUTPUT);
-  pinMode(motor1.pin2, OUTPUT);
-  pinMode(motor1.pinEn, OUTPUT);
+  // Configurar pines de control de motores.
+  setupMotor(motor1);
+  setupMotor(motor2);
 
-  pinMode(motor2.pin1, OUTPUT);
-  pinMode(motor2.pin2, OUTPUT);
-  pinMode(motor2.pinEn, OUTPUT);
+  setup_tcs3200(sensor_color);
 
   // Pin de activacion como entrada.
   pinMode(PIN_EN_MOV, INPUT);
@@ -65,6 +99,9 @@ void setup()
   if (usarMPU6050 && !mpu6050.begin())
   {
     Serial.println("Error de inicializacion del MPU6050: verifica que este conectado");
+
+    // Configurar el MPU6050.
+    configurarMPU6050();
   }
 
   Serial.println("MPU6050 inicializado");
@@ -75,12 +112,9 @@ void setup()
 void loop() 
 {
   // Revisar si se desean activar los motores
-  int movimientoActivado = HIGH;
+  int movimientoActivado = LOW;
   if (usarMPU6050)
   {
-    // Configurar el MPU6050.
-    configurarMPU6050();
-
     // Obtener valores del MPU6050.
     sensors_event_t a, g, t;
     mpu6050.getEvent(&a, &g, &t);
@@ -97,6 +131,12 @@ void loop()
       Serial.println("Temperatura: " + String(t.temperature) + " °C");
     }
   }
+
+  rgb_t color_detectado = medir_color(sensor_color);
+
+  Serial.println("Color: (R = " + String(color_detectado.r) + ", G = " + String(color_detectado.g) + ", B = " + String(color_detectado.b) + ")");
+
+  delay(1000);
 
   if (movimientoActivado == HIGH)
   {
@@ -118,8 +158,67 @@ void loop()
     // Detener motores por un segundo.
     cambiarMovimientoMotor(MOTOR_DETENIDO, motor1);
     cambiarMovimientoMotor(MOTOR_DETENIDO, motor2);
-    delay(1000);
   }
+}
+
+void setup_tcs3200(const tcs3200_t& sensor)
+{
+  pinMode(sensor.s0, OUTPUT);
+  pinMode(sensor.s1, OUTPUT);
+  pinMode(sensor.s2, OUTPUT);
+  pinMode(sensor.s3, OUTPUT);
+
+  // Usar escalado de filtro del 100%.
+  digitalWrite(sensor.s0, HIGH);
+  digitalWrite(sensor.s1, LOW);
+
+  pinMode(sensor.out, INPUT);
+}
+
+rgb_t medir_color(const tcs3200_t& sensor)
+{
+  // Activar el filtro rojo.
+  digitalWrite(sensor.s2, LOW);
+  digitalWrite(sensor.s3, LOW);
+
+  const int freq_rojo = pulseIn(sensor.out, LOW);
+
+  if (freq_rojo == 0) Serial.println("Timeout de medicion del canal rojo.");
+  
+  delay(25);
+  
+  // Activar el filtro verde.
+  digitalWrite(sensor.s2, HIGH);
+  digitalWrite(sensor.s3, HIGH);
+
+  const int freq_verde = pulseIn(sensor.out, LOW);
+  
+  if (freq_verde == 0) Serial.println("Timeout de medicion del canal verde.");
+
+  delay(25);
+
+  // Activar el filtro azul.
+  digitalWrite(sensor.s2, LOW);
+  digitalWrite(sensor.s3, HIGH);
+
+  const int freq_azul = pulseIn(sensor.out, LOW);
+
+  if (freq_azul == 0) Serial.println("Timeout de medicion del canal azul.");
+
+  // Limitar el valor de color a un rango entre 0 - VALOR_MAX_COLOR, con 0 como 
+  // intensidad minima y VALOR_MAX_COLOR como intensidad maxima del color.
+  const int rojo_corregido = VALOR_MAX_COLOR - constrain(freq_rojo, 0, VALOR_MAX_COLOR);
+  const int verde_corregido = VALOR_MAX_COLOR - constrain(freq_verde, 0, VALOR_MAX_COLOR);
+  const int azul_corregido = VALOR_MAX_COLOR - constrain(freq_azul, 0, VALOR_MAX_COLOR);
+
+  // Transformar colores a valores entre 0 y 255.
+  rgb_t colorDetectado = { 
+    map(rojo_corregido, 0, VALOR_MAX_COLOR, 0, 255), 
+    map(verde_corregido, 0, VALOR_MAX_COLOR, 0, 255),
+    map(azul_corregido, 0, VALOR_MAX_COLOR, 0, 255) 
+  };
+  
+  return colorDetectado;
 }
 
 // Esta funcion configura el rango del acelerometro, el rango del giroscopio y el
@@ -195,6 +294,15 @@ void configurarMPU6050()
   }
 }
 
+void setupMotor(const pines_motor_t& pinesMotor)
+{
+  ledcAttachPin(pinesMotor.pinEn, pinesMotor.canalPWM);
+  ledcSetup(pinesMotor.canalPWM, PWM1_FREQ, PWM1_RES);
+
+  pinMode(pinesMotor.pin1, OUTPUT);
+  pinMode(pinesMotor.pin2, OUTPUT);
+}
+
 // Definicion de la funcion cambiarMovimientoMotor.
 void cambiarMovimientoMotor(const int nuevoEstado, const pines_motor_t& pinesMotor, const int velocidad)
 {
@@ -205,19 +313,19 @@ void cambiarMovimientoMotor(const int nuevoEstado, const pines_motor_t& pinesMot
       // Detener completamente el motor.
       digitalWrite(pinesMotor.pin1, LOW);
       digitalWrite(pinesMotor.pin2, LOW);
-      analogWrite(pinesMotor.pinEn, 0);
+      ledcWrite(pinesMotor.canalPWM, 0);
       break;
     case MOTOR_AVANZANDO:
       // Hacer que el motor gire, con cierta velocidad.
       digitalWrite(pinesMotor.pin1, HIGH);
       digitalWrite(pinesMotor.pin2, LOW);
-      analogWrite(pinesMotor.pinEn, velocidad);
+      ledcWrite(pinesMotor.canalPWM, velocidad);
       break;
     case MOTOR_RETROCEDIENDO:
       // Hacer que el motor gire en sentido inverso, con cierta velocidad.
       digitalWrite(pinesMotor.pin1, LOW);
       digitalWrite(pinesMotor.pin2, HIGH);
-      analogWrite(pinesMotor.pinEn, velocidad);
+      ledcWrite(pinesMotor.canalPWM, velocidad);
       break;
     default: // El nuevoEstado recibido no es soportado, no hacer nada.
       return;
